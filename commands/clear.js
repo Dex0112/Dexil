0dd6d9632221f0d98b9a090a30e6813baa5824d2
Dex0112 <dexcrazy06@gmail.com>
2022-02-24 22:37:57 -0500

    Discord-Bot

--- commands/clear.js
+++ commands/clear.js
@@ -10,7 +10,7 @@ module.exports = {
         if(isNaN(args[0]))
             return message.reply("Please enter a real number!")
 
-        if(args[0] > 99) return message.reply("You cannot delete more than 100 mesages!");
+        if(args[0] > 99) return message.reply("You cannot delete more than 99 mesages!");
         if(args[0] < 1) return message.reply("You must delete at least one message!");
 
         await message.channel.messages.fetch({limit: args[0] + 1}).then(messages => {
