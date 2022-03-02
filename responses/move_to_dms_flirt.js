37a6531db15cee3c650b02d0bcda52bc3de9d1d2
Landon Tedrow <dexcrazy06@gmail.com>
2022-03-02 16:11:13 -0500

    Discord-Bot

--- responses/move_to_dms_flirt.js
+++ responses/move_to_dms_flirt.js
@@ -3,6 +3,7 @@ const helper = require("../helper.js");
 module.exports = {
     triggers: [
         "Dm me Dexil 😉",
+        "Dm me Dexil",
         "Dexil, lets move this to dms"
     ],
     responses: [
