aca5760c5e00c74bbadd0ef7ed1e4a8b73ba0f5a
Landon Tedrow <dexcrazy06@gmail.com>
2022-03-01 19:30:18 -0500

    Discord-Bot

---
+++ commands/about.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "about",
+    description: "About me!",
+    execute(message, args) {
+        message.reply(`Awesome, Witty, Well-Designed, Epic, Funny, Handsome, and Cool. This bot loves hexagons as much as it’s creator.`);
+    }
+}
