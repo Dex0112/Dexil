4c0a1ae10794467f6bab236c58762e28844cd12b
Landon Tedrow <dexcrazy06@gmail.com>
2022-03-25 23:58:32 -0400

    Stop

--- timed_events/420.js
+++ timed_events/420.js
@@ -7,6 +7,7 @@ module.exports = {
         minute: 20
     },
     async execute() {
+        return;
         const guild = await client.guilds.fetch('939667236786937896');
         guild.channels.cache.get('939667236786937898').send("Nice");
     }
