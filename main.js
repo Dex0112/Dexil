54d1b98b10ad587e03035eb4a89d901ffb493bff
Dex0112 <=>
2022-06-22 15:14:14 -0400

    Attempted fix to allow for meme posting

--- main.js
+++ main.js
@@ -117,12 +117,12 @@ async function isValidMessage(message) {
     const spamCheckRange = 5;
     const maxSpamCount = 2;
 
-    const unregulatedChannels = ['945730937432444998']
+    const unregulatedChannels = ['945730937432444998'];
     
     if(unregulatedChannels.includes(message.channel.id))
         return true;
 
-    if(message.content.length < minMessageLength && /^\d+$/.test(message.content) == false && message.attachments.size < 0)
+    if(message.content.length < minMessageLength && /^\d+$/.test(message.content) == false && message.attachments.size == 0)
         return false;
 
 
