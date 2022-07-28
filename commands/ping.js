03ca4430ad2c9244865b89b7508044c887e0f258
Dex0112 <=>
2022-07-28 04:43:56 -0400

    Removed test code

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,5 @@ module.exports = {
     description: 'Replies pong!',
     execute(message, args) {
         message.channel.send("pong!");
-
-        console.log(message.guild.roles.cache.get('939667236786937896').name);
     }
 }
