cde92a7ac81ecab64295861d41d8b12707de328c
Dex0112 <dexcrazy06@gmail.com>
2022-08-06 16:41:41 -0400

    Test

--- commands/displayProfilePicture.js
+++ commands/displayProfilePicture.js
@@ -5,9 +5,7 @@ module.exports = {
     description: "Displays the profile pic of a user (@ user or leave blank to display your own).",
     execute(message, args) {
         const target = message.mentions.users.first() || message.author;
-
         const response = new MessageEmbed().setImage(target.displayAvatarURL({dynamic: true}));
-
         message.reply({embeds: [response]});
     }
 }
