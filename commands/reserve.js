c5703d51da6204009b9321086cf5ae342ae10cc2
Dex0112 <=>
2022-08-07 22:03:09 -0400

    Added spectator option

--- commands/reserve.js
+++ commands/reserve.js
@@ -2,7 +2,7 @@ const { Permissions } = require("discord.js");
 
 module.exports = {
     name: "reserve",
-    description: "Creates a voice channel for a specified role. (-reserve @(role))",
+    description: "Creates a voice channel for a specified role. (-reserve @(role) (Spectators True/False). Ex: -reserve @Dev true (makes vc for devs and allows spectators))",
     execute: (message, args) => {
         const CHANNEL_MIN_LIFE = 1;
 
@@ -10,27 +10,38 @@ module.exports = {
 
         const role = message.mentions.roles.first();
 
+        const spectators = args[1]?.toLowerCase() == 'true' || false;
+
         if(!role)
             return message.reply("You must add a valid role as a parameter to reserve a voice channel!");
-
+        
         if(!message.member.roles.cache.has(role.id) && !message.member.roles.cache.has('939667378948681730'))
             return message.reply("You must have the role you want to reserve a voice channel for!")
+        
+        const permissionOverwrites = [
+            {
+                id: role.id,
+                allow: [Permissions.FLAGS.CONNECT]
+            }
+        ];
+
+        if(spectators) {
+            permissionOverwrites.push({
+                id: message.guild.id,
+                deny: [Permissions.FLAGS.SPEAK]
+            });
+        } else {
+            permissionOverwrites.push({
+                id: message.guild.id,
+                deny: [Permissions.FLAGS.CONNECT]
+            })
+        }
 
         try {
             message.guild.channels.create(role.name, {
                 type: "GUILD_VOICE",
                 parent: category,
-                permissionOverwrites: [
-                    {
-                        id: message.guild.id,
-                        allow: [Permissions.FLAGS.VIEW_CHANNEL],
-                        deny: [Permissions.FLAGS.CONNECT]
-                    },
-                    {
-                        id: role.id,
-                        allow: [Permissions.FLAGS.CONNECT],
-                    }
-                ]
+                permissionOverwrites: permissionOverwrites
             }).then(channel => {
                 message.reply(`You have ${CHANNEL_MIN_LIFE} minutes to join ${channel}!`);
 
