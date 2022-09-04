f1aa1399535283d1eaaf465a902ddc17ce8b8705
Dex0112 <=>
2022-09-04 16:42:44 -0400

    Cleaned up if statements

--- commands/clear.js
+++ commands/clear.js
@@ -7,11 +7,8 @@ module.exports = {
         if(!Permissions.hasPermission(message.member, Permissions.MANAGE_MESSAGES))
             return message.reply("You do not have permission to complete this command!");
 
-        if(!args[0]) 
-            args[0] = 1;
-        if(isNaN(args[0]))
-            return message.reply("Please enter a real number!")
-
+        if(!args[0]) args[0] = 1;
+        if(isNaN(args[0])) return message.reply("Please enter a real number!")
         if(args[0] > 99) return message.reply("You cannot delete more than 99 mesages!");
         if(args[0] < 1) return message.reply("You must delete at least one message!");
 
