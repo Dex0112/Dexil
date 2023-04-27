ef6b6039240733486d4d0fea1bfc3f421b949fb5
Dex0112 <=>
2023-04-27 01:04:51 -0400

    new guild member

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,8 +1,7 @@
 module.exports = (Discord, client, member) => {
-    member.roles.add('946977499647197245');
-    member.roles.add('989003511968714812');
+    member.roles.add('1101010432438841397');
 
-    member.guild.channels.cache.get('939667236786937898').send(`Welcome to TB (not tuberculosis) ${member}`);
+    member.guild.channels.cache.get('1100606978755145761').send(`Welcome to TB (not tuberculosis) ${member}`);
     member.user.send("GET OUT WHILE YOU STILL CAN!!!");
     setTimeout(() => {
         member.user.send("But, if you are going to stay, in the server, you can do ``-role`` to see a list of the available roles and ``-help`` to get a list of all the commands with descriptions of what they do. Enjoy the server!");
